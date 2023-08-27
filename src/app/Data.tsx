17ae04664f58985e3b10324bc2300bf7d1e94f7b
Rafael Reis Franco <rafaelreisfranco15@gmail.com>
2023-08-27 17:57:55 -0300

    first commit

---
+++ src/app/Data.tsx
@@ -0,0 +1,38 @@
+export const UserData = [
+    {
+        id: 1,
+        year: 2002,
+        userGain: 8000,
+        userLost: 822,
+    },
+    {
+        id: 2,
+        year: 2032,
+        userGain: 3431,
+        userLost: 222,
+    },
+    {
+        id: 2,
+        year: 2003,
+        userGain: 1500,
+        userLost: 531,
+    },
+    {
+        id: 3,
+        year: 1900,
+        userGain: 43123,
+        userLost: 432,
+    },
+    {
+        id: 4,
+        year: 2022,
+        userGain: 1000,
+        userLost: 322,
+    },
+    {
+        id: 5,
+        year: 1932,
+        userGain: 50000,
+        userLost: 313,
+    },
+]
